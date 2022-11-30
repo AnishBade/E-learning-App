c333766ced3ba26474694ce57c9e1768a694f8bd
AnishBade <badeanish6045@gmail.com>
2022-11-30 19:28:37 +0545

    Adding content to course modules

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+
+
+
 from django.views.generic.list import ListView 
 from .models import Course
 from django.urls import reverse_lazy 
@@ -105,3 +108,11 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
                 Content.objects.create(module=self.module, item=obj)
             return redirect('module_content_list', self.module.id)
         return self.render_to_response({'form': form,'object': self.obj})
+
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(Content, id=id,module__course__owner=request.user)
+        module = content.module 
+        content.item.delete() 
+        content.delete()
+        return redirect('module_content_list', module.id)
