f9370495f603a04db3706b5a30503077cd19768f
AnishBade <badeanish6045@gmail.com>
2023-01-10 13:35:31 +0545

    Adding Students Registration

--- courses/views.py
+++ courses/views.py
@@ -16,6 +16,8 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count 
 from .models import Subject
 from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
+
 # Create your views here.
 class ManageCourseListView(ListView): 
     model = Course
@@ -149,3 +151,7 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView): 
     model = Course
     template_name = 'courses/course/detail.html'
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context['enroll_form'] = CourseEnrollForm(initial={'course':self.object}) 
+        return context
